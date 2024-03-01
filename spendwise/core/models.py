4b9fb65df9b80f9f5f382822e548efb66925c526
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-01 13:58:56 -0300

    feature(core): adds Category model

--- spendwise/core/models.py
+++ spendwise/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Name")
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return f"{self.name}"
