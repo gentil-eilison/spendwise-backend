72a6de7fc86832c916cf41ac7c2b6511a03e238a
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-01 17:08:04 -0300

    feature(core): adds load_db_data command

---
+++ spendwise/core/management/commands/load_db_data.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+
+from spendwise.core.models import Category
+
+
+class Command(BaseCommand):
+    help = "Creates categories"
+
+    def handle(self, *args, **kwargs):
+        categories_names = (
+            "Housing",
+            "Transportation",
+            "Food",
+            "Utilities",
+            "Insurance",
+            "Medial & Healthcare",
+            "Saving",
+            "Investing",
+            "Debt Payment",
+            "Personal Spending",
+            "Entertainment",
+            "Miscellaneous"
+        )
+
+        for name in categories_names:
+            Category.objects.get_or_create(
+                name=name
+            )
+        self.stdout.write(self.style.SUCCESS("Categories successfully created"))
